6137e0a2512e2114d4dfc99b55a9b7fc93d4f5be
Alexey Dmitriev <lesha1599@mail.ru>
2024-08-30 16:02:25 +0300

    Update

--- PDF-Splitter/Splitter.cs
+++ PDF-Splitter/Splitter.cs
@@ -1,9 +1,14 @@
-﻿using Spire.Pdf;
-using Spire.Pdf.Graphics;
-using System;
+﻿using System;
 using System.Drawing; // для ImageFormat
 using System.Drawing.Imaging; // для ImageFormat
 using System.IO;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf.IO;
+using PdfSharp.UniversalAccessibility.Drawing;
+using System.Windows;
+using iText.IO.Image;
+using ImageMagick;
 
 namespace PDF_Splitter
 {
@@ -11,22 +16,65 @@ namespace PDF_Splitter
     {
         public static void SplitPdfToImages(string pdfPath, string outputPath)
         {
-            //Создайте экземпляр PdfDocument
-            PdfDocument pdf = new PdfDocument();
-            //Загрузка образца документа PDF
-            pdf.LoadFromFile(pdfPath);
-            //Просматривайте каждую страницу в PDF
-            for (int i = 0; i < pdf.Pages.Count; i++)
+
+            using (MagickImageCollection images = new MagickImageCollection())
             {
-                //Преобразуйте все страницы в изображения и установите разрешение на дюйм для изображений
-                Image image = pdf.SaveAsImage(i, PdfImageType.Bitmap, 500, 500);
-                //Сохранить изображения в формате PNG в указанную папку 
-                String file = String.Format(outputPath + "\\ToImage-{0}.png", i);
-                image.Save(file, ImageFormat.Png);
-                //Сохранить изображения в формате JPG в указанную папку 
-                //String fileJpg = String.Format("\\ToImage-{0}.jpg", i);
-                //image.Save(fileJpg, ImageFormat.Jpeg);
+                images.Read(pdfPath);
+                using (IMagickImage vertical = images.AppendVertically())
+                {
+                    vertical.Format = MagickFormat.Png;
+                    vertical.Density = new Density(300);
+                    vertical.Write(outputPath);
+                }
             }
+
+            //using (var pdfDocument = PdfReader.Open(pdfPath, PdfDocumentOpenMode.Import))
+            //{
+            //    var pdfPage = pdfDocument.Pages[0];
+            //    using (var bitmap = new Bitmap((int)pdfPage.Width, (int)pdfPage.Height))
+            //    {
+            //        using (var graphics = Graphics.FromImage(bitmap))
+            //        {
+            //            //graphics.DrawImage(pdfPage, System.Drawing.Point.Empty);
+            //            // Render page content here...
+            //            //pdfPage.Render();
+            //            pdfPage.Contents.
+            //        }
+            //        bitmap.Save(outputPath, ImageFormat.Png);
+            //    }
+
+            //}
+
+            //string srcPDF = pdfPath;
+            //PdfDocument pdfd = PdfReader.Open(srcPDF);
+            //XGraphics xgfx = XGraphics.FromPdfPage(pdfd.Pages[0]);
+            //Bitmap b = new Bitmap((int)1920, (int)1080, xgfx.From);
+
+            //// Открываем PDF-файл
+            //PdfDocument document = PdfReader.Open(pdfPath);
+
+            //// Получаем первую страницу
+            //PdfPage page = document.Pages[0];
+
+            //// Создаем XGraphics для отрисовки на странице
+            //XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            //// Определяем размер страницы
+            //int width = (int)page.Width.Point;
+            //int height = (int)page.Height.Point;
+
+            //// Создаем Bitmap для отрисовки страницы
+            //using (Bitmap bitmap = new Bitmap(width, height))
+            //{
+            //    using (Graphics g = Graphics.FromImage(bitmap))
+            //    {
+            //        // Отрисовываем страницу на Bitmap
+            //        g.DrawImage(xImage, 0, 0, width, height);
+
+            //        // Сохраняем Bitmap в файл
+            //        bitmap.Save(imagePath, ImageFormat.Jpeg);
+            //    }
+            //}
         }
     }
 }
