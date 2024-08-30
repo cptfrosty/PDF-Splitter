67f5aa91fb84921898bc401e236615506f638086
Alexey <lesha1599@mail.ru>
2024-08-30 07:39:59 +0300

    Initial commit

---
+++ PDF-Splitter/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Xml.Linq;
+using System.Windows.Forms;
+
+
+namespace PDF_Splitter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> pathFiles = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImagePanel_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(System.Windows.DataFormats.FileDrop);
+
+                foreach(string file in files)
+                {
+                    pathFiles.Add(file);
+                }
+
+                foreach (string file in files)
+                {
+                    // Проверяем, является ли файл PDF
+                    if (System.IO.Path.GetExtension(file).ToLower() == ".pdf")
+                    {
+                        string nameFile = GetFileNameFromPath(file);
+                        CreateFileVisual(nameFile);
+                    }
+                    else
+                    {
+                        // Выводим сообщение об ошибке, если файл не PDF
+                        System.Windows.MessageBox.Show("Можно перетаскивать только PDF файлы!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public string GetFileNameFromPath(string filePath)
+        {
+            // Разбиваем путь на части, используя разделитель пути
+            string[] parts = filePath.Split(System.IO.Path.DirectorySeparatorChar);
+
+            // Возвращаем последний элемент массива - имя файла
+            return parts[parts.Length - 1];
+        }
+
+        private void CreateFileVisual(string nameFile)
+        {
+            Grid grid = new Grid();
+            RowDefinition rowImage = new RowDefinition();
+            RowDefinition rowText = new RowDefinition();
+            System.Windows.Controls.Label label = new System.Windows.Controls.Label();
+            System.Windows.Controls.Image pdfImage = new System.Windows.Controls.Image();
+
+            grid.Height = 100;
+            grid.Width = 100;
+
+            pdfImage.Source = new BitmapImage(new Uri("Images/pdf.png", UriKind.Relative)); ;
+
+            rowImage.Height = new GridLength(70);
+
+            label.Content = nameFile;
+
+            grid.RowDefinitions.Add(rowImage);
+            grid.RowDefinitions.Add(rowText);
+
+            Grid.SetRow(pdfImage, 0);
+            Grid.SetRow(label, 1);
+
+            grid.Children.Add(label);
+            grid.Children.Add(pdfImage);
+            ImagePanel.Children.Add(grid);
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем диалоговое окно для выбора папки
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Выберите папку";
+            string folderPath = "";
+            // Показывает диалоговое окно
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Получаем путь к выбранной папке
+                folderPath = folderBrowserDialog.SelectedPath;
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+            }
+
+            if(folderPath != "")
+            {
+                foreach(var pathFile in pathFiles)
+                {
+                    Splitter.SplitPdfToImages(pathFile, folderPath + "//" + GetFileNameFromPath(pathFile));
+                }
+            }
+        }
+    }
+}
---
+++ PDF-Splitter/Splitter.cs
@@ -0,0 +1,32 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using System;
+using System.Drawing; // для ImageFormat
+using System.Drawing.Imaging; // для ImageFormat
+using System.IO;
+
+namespace PDF_Splitter
+{
+    internal class Splitter
+    {
+        public static void SplitPdfToImages(string pdfPath, string outputPath)
+        {
+            //Создайте экземпляр PdfDocument
+            PdfDocument pdf = new PdfDocument();
+            //Загрузка образца документа PDF
+            pdf.LoadFromFile(pdfPath);
+            //Просматривайте каждую страницу в PDF
+            for (int i = 0; i < pdf.Pages.Count; i++)
+            {
+                //Преобразуйте все страницы в изображения и установите разрешение на дюйм для изображений
+                Image image = pdf.SaveAsImage(i, PdfImageType.Bitmap, 500, 500);
+                //Сохранить изображения в формате PNG в указанную папку 
+                String file = String.Format(outputPath + "\\ToImage-{0}.png", i);
+                image.Save(file, ImageFormat.Png);
+                //Сохранить изображения в формате JPG в указанную папку 
+                //String fileJpg = String.Format("\\ToImage-{0}.jpg", i);
+                //image.Save(fileJpg, ImageFormat.Jpeg);
+            }
+        }
+    }
+}
